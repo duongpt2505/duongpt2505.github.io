3be4a6e5d9f40344830d0dc4a957af9059d27e85
duongpt2505 <50900484@hcmut.edu.vn>
2021-06-20 22:14:12 +0700

    Add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'duongpt2505.github.io',
+    title: 'Hàm Yên Tam Kỳ - Chuyên ảnh baby & gia đình',
     htmlAttrs: {
       lang: 'en'
     },
@@ -14,7 +14,7 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/fav.ico' }
     ]
   },
 
